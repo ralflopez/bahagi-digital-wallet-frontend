8de2272b075b2e4fa3b8e2953ca057fc4610397f
Ralf <ralflopez3@gmail.com>
2022-05-21 13:06:16 +0800

    :wrench: configure initial apollo client settings

---
+++ graphql/client.ts
@@ -0,0 +1,30 @@
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client"
+import cookie from "cookie"
+import nookies from "nookies"
+
+export const client = new ApolloClient({
+  link: createHttpLink({
+    uri: "http://localhost:4000/graphql",
+    credentials: "include",
+  }),
+  cache: new InMemoryCache(),
+  ssrMode: typeof window === "undefined",
+})
+
+export const clientSSR = (context: any) => {
+  const KEY = "connect.sid"
+  const cookies = nookies.get(context)
+  const sessionId = cookies[KEY]
+
+  return new ApolloClient({
+    link: createHttpLink({
+      uri: "http://localhost:4000/graphql",
+      credentials: "include",
+      headers: {
+        Cookie: cookie.serialize(KEY, sessionId),
+      },
+    }),
+    cache: new InMemoryCache(),
+    ssrMode: true,
+  })
+}
