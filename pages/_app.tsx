eeaf147b8bf241e56bcf590341b249f5b45f3a69
Ralf <ralflopez3@gmail.com>
2022-06-10 00:38:57 +0800

    :lipstick: use custom favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,14 @@ import type { AppProps } from "next/app"
 import { ChakraProvider } from "@chakra-ui/react"
 import { ApolloProvider } from "@apollo/client"
 import { client } from "../graphql/client"
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
+      <Head>
+        <title>E-Wallet</title>
+      </Head>
       <ChakraProvider>
         <Component {...pageProps} />
       </ChakraProvider>
